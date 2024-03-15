733574fbff25f81ec7d4c98a2aae1d1a989eef05
proitlab <dedy.sutanto@proit.co.id>
2024-03-15 15:07:21 +0700

    Remove Upload License replace with component

--- licenses/urls.py
+++ licenses/urls.py
@@ -3,7 +3,7 @@ from .views import upload_view, license_handler, license_download
 
 
 urlpatterns = [
-        path('upload/', upload_view, name='upload_view'),
+        #path('upload/', upload_view, name='upload_view'),
         path('handler/', license_handler, name='license_handler'),
         path('download/<int:license_id>/', license_download, name='license_download'),
         ]
